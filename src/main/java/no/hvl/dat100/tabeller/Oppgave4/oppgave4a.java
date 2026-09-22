f6597df9f96564413ceea285880cefc027ef8f12
678062 <318213714+678062@users.noreply.github.com>
2026-09-22 19:38:59 +0200

    Update oppgave4a.java

--- src/main/java/no/hvl/dat100/tabeller/Oppgave4/oppgave4a.java
+++ src/main/java/no/hvl/dat100/tabeller/Oppgave4/oppgave4a.java
@@ -1,5 +1,14 @@
 package no.hvl.dat100.tabeller.Oppgave4;
 
 public class oppgave4a {
+<<<<<<< Updated upstream
     //hei
+=======
+    public static void skrivUt(int[] tabell)
+
+    for( int i = 0; i<tab.length;i++)
+            System.out.print(tab[i]);
+
+    }
+>>>>>>> Stashed changes
 }
